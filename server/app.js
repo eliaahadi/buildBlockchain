e24c7c91c2e76d554ab5680402edfbaf5ce489f4
eliaahadi <elia.ahadi@gmail.com>
2018-04-15 20:54:55 +0900

    fav path

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-var favicon = require('serve-favicon');
+const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
@@ -8,7 +8,8 @@ const bodyParser = require('body-parser');
 const index = require('./routes/index');
 
 const app = express();
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname + '/public/favicon.ico')));
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
