285c2f92d9061488fdf1efdd4d9b7fc616343f48
eliagitai <elia.ahadi@gmail.com>
2019-07-22 16:17:00 +0900

    updated c# blockchain

---
+++ c#blockchain/nl.hyperdata.blockchain.test/blockchainTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace nl.hyperdata.blockchain.test
+{
+    [TestClass]
+    public class BlockChainTest
+    {
+        private static Random random = new Random(DateTime.Now.Millisecond);
+        private static readonly IBlock genesis = new Block(new byte[] { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 });
+        private static readonly byte[] difficulty = new byte[] { 0x00, 0x00 };
+
+
+        BlockChain chain = new BlockChain(difficulty, genesis);
+
+        [TestMethod]
+        public void TestBlockChainConstructor()
+        {
+            Assert.IsNotNull(chain);
+        }
+
+        [TestMethod]
+        public void TestBlockChainGenesisBlock()
+        {
+            Assert.AreEqual(1, chain.Items.Count);
+        }
+
+
+        [TestMethod]
+        public void TestBlockChainNewBlock()
+        {
+            var data = Enumerable.Range(0, 256).Select(x => (byte)random.Next());
+            chain.Add(new Block(data.ToArray()));
+
+            Assert.AreEqual(2, chain.Items.Count);
+        }
+
+
+    }
+}
---
+++ c#blockchain/nl.hyperdata.blockchain/Block.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace nl.hyperdata.blockchain
+{
+    public class Block : IBlock
+    {
+        public Block(byte[] data)
+        {
+            Data = data ?? throw new ArgumentNullException(nameof(data));
+            Nonce = 0;
+            PreviousHash = new byte[] { 0x00 };
+            TimeStamp = DateTime.Now;
+
+        }
+        public byte[] Data { get; }
+
+        public byte[] Hash { get; set; }
+
+        public byte[] PreviousHash { get; set; }
+
+        public int Nonce { get; set; }
+
+        public DateTime TimeStamp { get; }
+
+        public override string ToString()
+        {
+            return String.Format("{0}:\n:{1}:\n:{2}:\n{3}\n\r", BitConverter.ToString(Hash).Replace("-", ""), BitConverter.ToString(PreviousHash).Replace("-", ""), Nonce, TimeStamp);
+        }
+    }
+}
